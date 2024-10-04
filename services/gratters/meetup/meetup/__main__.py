ea0935c40a16e315a2b7657e1d071378efd1036d
Nicolas de Garrigues <ndegarrigues@gmail.com>
2024-10-04 16:32:43 +0200

    feat: add pagination research

--- services/gratters/meetup/meetup/__main__.py
+++ services/gratters/meetup/meetup/__main__.py
@@ -1,9 +1,7 @@
-import datetime
-from textwrap import indent
-
 from checks.checks import speak_food
 from gratus import run_and_send
-from meetup.gql import check, query
+from meetup.collector import collect
+from meetup.gql import check
 from meetup.parser import parse
 from models import Event
 
@@ -11,52 +9,9 @@ from models import Event
 def main() -> list[Event]:
     check()
     
-    now = datetime.datetime.now()
-    inOneWeek = now + datetime.timedelta(weeks=1)
-    
-    params = {"filter":{
-        "lat": "48.86000061035156",
-        "lon": "2.3399999141693115",
-        "startDateRange": f"{now.strftime('%Y-%m-%dT%H:%M:%S')}-04:00",
-        "endDateRange": f"{inOneWeek.strftime('%Y-%m-%dT%H:%M:%S')}-04:00",
-        "eventType": "PHYSICAL",
-    }}
-    result = query('''   
-          query test($filter: RecommendedEventsFilter!){
-              recommendedEvents(filter: $filter){
-                 pageInfo {
-                 hasNextPage
-                 endCursor
-            }
-                edges {
-                    node {
-                        id
-                        title
-                        description
-                        eventUrl
-                        venue {
-                            name
-                            address
-                            city
-                            eventVenueOptions {
-                                feeSettings {
-                                    amount
-                                }
-                            }
-                        }
-                        dateTime
-                        endTime
-                    }
-                }
-              }
-          }  
-    ''', params)
-    
-    print(result["recommendedEvents"]['pageInfo'])
-    
     ret : list[Event] = []
-    for d in result['recommendedEvents']['edges']:
-        if (evt:=parse(d['node'])) is not None:
+    for d in collect():
+        if (evt:=parse(d)) is not None:
             if speak_food(evt.model_dump_json()):
                 print(f'Event {evt.name} is accepted')
                 ret.append(evt)
